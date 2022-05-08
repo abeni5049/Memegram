ef992e2b9b9f9b9da44f3ac185222131b735afdc
abeni <abeni5049@gmail.com>
2022-05-08 12:44:42 +0300

    created bottom navigation bar

--- app/src/main/java/com/example/memegram/LoginActivity.java
+++ app/src/main/java/com/example/memegram/LoginActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
     TextView registerText;
+    Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +19,12 @@ public class LoginActivity extends AppCompatActivity {
         registerText = findViewById(R.id.register_text);
         registerText.setOnClickListener(view -> {
             Intent i = new Intent(LoginActivity.this,SignupActivity.class);
+            startActivity(i);
+        });
+
+        loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(view -> {
+            Intent i = new Intent(LoginActivity.this,MainActivity.class);
             startActivity(i);
         });
     }
