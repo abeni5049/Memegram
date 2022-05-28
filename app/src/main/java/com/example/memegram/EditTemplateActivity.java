bbaf5e2b0688f8b9fb120710d7de15d268aebb69
abeni <abeni5049@gmail.com>
2022-05-28 12:16:10 +0300

    added save button to save the created meme

--- app/src/main/java/com/example/memegram/EditTemplateActivity.java
+++ app/src/main/java/com/example/memegram/EditTemplateActivity.java
@@ -14,32 +14,96 @@ import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Environment;
+import android.text.Editable;
 import android.text.Layout;
 import android.text.StaticLayout;
 import android.text.TextPaint;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class EditTemplateActivity extends AppCompatActivity {
-    private final int TEXT_MAX_LENGTH = 71;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_template);
         ImageView template_image = findViewById(R.id.template_image);
+        EditText topEditText = findViewById(R.id.top_edit_text);
+        EditText bottomEditText = findViewById(R.id.bottom_edit_text);
+        Button saveButton = findViewById(R.id.save_button);
+
         int ImageURL = getIntent().getExtras().getInt("ImageURL");
         template_image.setImageResource(ImageURL);
+
         Bitmap template_image_bitmap = ((BitmapDrawable)template_image. getDrawable()). getBitmap();
         Bitmap bitmap1 = addPaddingTopForBitmap(template_image_bitmap,100);
         Bitmap bitmap2 = addPaddingBottomForBitmap(bitmap1,100);
         Bitmap bitmap3 = addPaddingLeftForBitmap(bitmap2,0);
         Bitmap bitmap4 = addPaddingRightForBitmap(bitmap3,0);
-        String textTop = "my first meme I'm so excited to post this meme";
-        String textBottom = "it means a lot to me";
-        Bitmap bitmap5 = drawMultilineTextToBitmapTop(getApplicationContext(),bitmap4,textBottom);
-        Bitmap bitmap6= drawMultilineTextToBitmapBottom(getApplicationContext(),bitmap5,textBottom);
-        template_image.setImageBitmap(bitmap6);
+
+        topEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String textTop = topEditText.getText().toString();
+                String textBottom = bottomEditText.getText().toString();
+                Bitmap bitmap5 = drawMultilineTextToBitmapTop(getApplicationContext(),bitmap4,textTop);
+                Bitmap bitmap6= drawMultilineTextToBitmapBottom(getApplicationContext(),bitmap5,textBottom);
+                template_image.setImageBitmap(bitmap6);
+
+            }
+        });
+
+        bottomEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String textTop = topEditText.getText().toString();
+                String textBottom = bottomEditText.getText().toString();
+                Bitmap bitmap5 = drawMultilineTextToBitmapTop(getApplicationContext(),bitmap4,textTop);
+                Bitmap bitmap6= drawMultilineTextToBitmapBottom(getApplicationContext(),bitmap5,textBottom);
+                template_image.setImageBitmap(bitmap6);
+            }
+        });
+
+        saveButton.setOnClickListener(View->{
+            try{
+                String path = Environment.getExternalStorageDirectory().toString();
+                File file = new File(path, "meme.png");
+                FileOutputStream out = new FileOutputStream(file);
+                template_image_bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+                out.flush();
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
 
     }
 
